56f11cb834dcbb018fdf8dcd813dd4b715f02340
Antoine <ant.g@hotmail.be>
2021-02-06 13:15:13 +0100

    feed card

--- frontend/app/screens/SearchScreen/index.js
+++ frontend/app/screens/SearchScreen/index.js
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { View, Image, Text, TextInput, FlatList } from 'react-native';
-import Axios from 'axios';
-import filter from 'lodash.filter';
-import { useIsFocused } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from "react";
+import { View, Image, Text, TextInput, FlatList } from "react-native";
+import Axios from "axios";
+import filter from "lodash.filter";
+import { useIsFocused } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 // Local imports
-import logo from '../../assets/logo.png';
-import styles from './styles';
-import EventsTemplate from '../../components/EventsTemplate/index.js';
-import ipAdd from '../../utils/ipAdd.js';
+import logo from "../../assets/logo.png";
+import styles from "./styles";
+import EventsTemplate from "../../components/EventsTemplate/index.js";
+import CardFeed from "../../components/CardFeed/index.js";
+import ipAdd from "../../utils/ipAdd.js";
+import feed from "../../utils/feed.js";
+import { ScrollView } from "react-native-gesture-handler";
 
 const SearchScreen = () => {
   const navigation = useNavigation();
 
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [fullData, setFullData] = useState([]);
   const [data, setData] = useState([]);
   const isFocused = useIsFocused();
@@ -23,19 +26,19 @@ const SearchScreen = () => {
 
   useEffect(() => {
     Axios.get(API_ENDPOINT)
-      .then(response => {
+      .then((response) => {
         setData(response.data);
         setFullData(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, [isFocused]);
 
-  const handleSearch = text => {
+  const handleSearch = (text) => {
     setQuery(text);
     const formattedQuery = text.toLowerCase();
-    const filteredData = filter(fullData, item => {
+    const filteredData = filter(fullData, (item) => {
       return contains(item, formattedQuery);
     });
     setData(filteredData);
@@ -52,6 +55,10 @@ const SearchScreen = () => {
     return <EventsTemplate id={item.id} event={item} />;
   };
 
+  const maping = () => {
+    return feed.map((feed) => <CardFeed key={feed.key} feed={feed} />);
+  };
+
   const RenderHeader = () => {
     return (
       <View style={styles.inputBox}>
@@ -60,8 +67,8 @@ const SearchScreen = () => {
           autoCorrect={false}
           clearButtonMode="always"
           value={query}
-          onChangeText={queryText => handleSearch(queryText)}
-          placeholder="Looking for an event?"
+          onChangeText={(queryText) => handleSearch(queryText)}
+          placeholder="What's on your mind?"
           style={styles.input}
         />
       </View>
@@ -69,29 +76,19 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.logo} source={logo} />
-      <Text style={styles.filter}>My search</Text>
-      <RenderHeader />
-      <FlatList
-        data={data}
-        keyExtractor={item => item.title}
-        renderItem={mapingEventDB}
-        showsVerticalScrollIndicator={false}
-      />
-
-      {/* <View style={styles.inputBox}>
-        <TextInput 
-          style={styles.input}
-          type="text"
-          onChange={handleChange}
-          placeholder="Looking for an event?"
-          placeholderTextColor="gray"
-          clearButtonMode="always"
-          value={input}
-          >
-        </TextInput>  
-      </View> */}
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        <Text style={styles.filter}>My Feed</Text>
+        <RenderHeader />
+        {/* <FlatList
+          data={data}
+          keyExtractor={(item) => item.title}
+          renderItem={mapingEventDB}
+          showsVerticalScrollIndicator={false}
+        /> */}
+      </View>
+      <ScrollView>{maping()}</ScrollView>
     </View>
   );
 };
